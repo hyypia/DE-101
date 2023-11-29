83d50c35c970594b720f3ac3913b5c67ebf5a90f
igor <igortereschuk92@gmail.com>
2023-11-29 17:06:38 +0200

    DDL for superstore dimensional model created

--- Module02/superstore_dimensional_model.sql
+++ Module02/superstore_dimensional_model.sql
@@ -1,3 +1,6 @@
+-- Dimensional Tables
+
+DROP TABLE IF EXISTS Customer_dim;
 CREATE TABLE Customer_dim
 (
  customer_id   varchar(50) NOT NULL,
@@ -6,6 +9,7 @@ CREATE TABLE Customer_dim
  CONSTRAINT PK_1 PRIMARY KEY (customer_id)
 );
 
+DROP TABLE IF EXISTS Address_dim;
 CREATE TABLE Address_dim
 (
  address_id  int NOT NULL,
@@ -14,9 +18,10 @@ CREATE TABLE Address_dim
  state       varchar(50) NOT NULL,
  city        varchar(50) NOT NULL,
  postal_code int NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( address_id )
+ CONSTRAINT PK_1 PRIMARY KEY (address_id)
 );
 
+DROP TABLE IF EXISTS Date_dim;
 CREATE TABLE Date_dim
 (
  date_id int NOT NULL,
@@ -24,25 +29,31 @@ CREATE TABLE Date_dim
  day     int NOT NULL,
  month   int NOT NULL,
  year    int NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( date_id )
+ CONSTRAINT PK_1 PRIMARY KEY (date_id)
 );
 
+DROP TABLE IF EXISTS Product_dim;
 CREATE TABLE Product_dim
 (
  product_id   varchar(50) NOT NULL,
  product_name text NOT NULL,
  category     varchar(50),
  subcategory  varchar(50),
- CONSTRAINT PK_1 PRIMARY KEY ( product_id )
+ CONSTRAINT PK_1 PRIMARY KEY (product_id)
 );
 
+DROP TABLE IF EXISTS Ship_dim;
 CREATE TABLE Ship_dim
 (
  ship_id   int NOT NULL,
  ship_mode varchar(50) NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( ship_id )
+ CONSTRAINT PK_1 PRIMARY KEY (ship_id)
 );
 
+
+-- Fact Table
+
+DROP TABLE IF EXISTS Sale_fact;
 CREATE TABLE Sale_fact
 (
  row_id      int NOT NULL,
@@ -56,10 +67,10 @@ CREATE TABLE Sale_fact
  quantity    int NOT NULL,
  discount    numeric(4, 2) NOT NULL,
  profit      numeric(9, 4) NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( row_id ),
- CONSTRAINT FK_1 FOREIGN KEY ( customer_id ) REFERENCES Customer_dim ( customer_id ),
- CONSTRAINT FK_3 FOREIGN KEY ( ship_id ) REFERENCES Ship_dim ( ship_id ),
- CONSTRAINT FK_3_1 FOREIGN KEY ( product_id ) REFERENCES Product_dim ( product_id ),
- CONSTRAINT FK_4 FOREIGN KEY ( date_id ) REFERENCES Date_dim ( date_id ),
- CONSTRAINT FK_5 FOREIGN KEY ( address_id ) REFERENCES Address_dim ( address_id )
+ PRIMARY KEY (row_id),
+ FOREIGN KEY (customer_id) REFERENCES Customer_dim (customer_id),
+ FOREIGN KEY (ship_id) REFERENCES Ship_dim (ship_id),
+ FOREIGN KEY (product_id) REFERENCES Product_dim (product_id),
+ FOREIGN KEY (date_id) REFERENCES Date_dim (date_id),
+ FOREIGN KEY (address_id) REFERENCES Address_dim (address_id)
 );
